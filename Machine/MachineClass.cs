95eb6b49e8d3f5071561a6bc2ab59afe6104771a
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2022-01-11 01:00:17 +0700

    Menambahkan beberapa function kompleks

--- Machine/MachineClass.cs
+++ Machine/MachineClass.cs
@@ -3,19 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VendingMachine.Models;
 
 namespace VendingMachine.Machine
 {
     class MachineClass
     {
-        public void Pembelian(int uang, int id, int qty)
+        public List<MakananModel> GetMakananList(List<MakananModel> MakananList, int id)
         {
-            MakeList GetList = new MakeList();
-            //var
+            var GetMakanan = MakananList.Where(ss => ss.id == id).ToList();
+            return GetMakanan;
         }
 
-        // function Pembelian
-        // function Pengembalian
-        // function Cek Stok 
+        public string PengecekanStok(List<MakananModel> MakananList, int id, int stok)
+        {
+            var GetMakanan = GetMakananList(MakananList, id).FirstOrDefault();
+            int Total = GetMakanan.harga * stok;
+            string Val = GetMakanan.stok >= stok ? 
+                        "Stok Makanan Tersedia\nTotal Pemesanan Rp. "+ Total : 
+                        "Stok Makanan Kurang";
+            return Val;
+        }
+
+        public bool PengecekanUang(List<UangPecahanModel> uangPecahanList, int uang)
+        {
+            bool Val = uangPecahanList.Any(ss => ss.pecahan == uang);
+            return Val;
+        }
+
+        public int TotalPemesanan(List<MakananModel> MakananList, int id, int stok)
+        {
+            var GetMakanan = GetMakananList(MakananList, id).FirstOrDefault();
+            return GetMakanan.harga * stok;
+        }
+
+        public int Pembelian(List<MakananModel> MakananList, int id, int stok, int uang)
+        {
+            var GetMakanan = GetMakananList(MakananList, id).FirstOrDefault();
+            var kembalian = uang - (GetMakanan.harga * stok);
+            return kembalian;
+        }
+
+        public List<MakananModel> UpdateStok(List<MakananModel> MakananList, int id, int stok)
+        {
+            MakananList.Where(ss => ss.id == id).ToList().ForEach(ss => ss.stok = ss.stok - stok);
+            return MakananList;
+        }
+
+        public void Report(List<MakananModel> MakananList, int id, int stok, int uang)
+        {
+            Console.WriteLine("---------- Pembayaran ----------");
+            Console.WriteLine("Pembayaran : " + uang);
+            Console.WriteLine("Total Belanja : " + TotalPemesanan(MakananList,id,stok));
+            Console.WriteLine("Kembalian : " + Pembelian(MakananList, id, stok, uang));
+            Console.WriteLine("---------- Terima Kasih ----------");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using VendingMachine.Machine;
 
 namespace VendingMachine
 {
@@ -6,19 +7,85 @@ namespace VendingMachine
     {
         static void Main(string[] args)
         {
-            int cek = 10;
-            string key;
-            Console.WriteLine(cek);
+            string status;
+            int NoMakanan, JmlPembelian, TotalPemesanan, Uang = 0;
+            bool condition;
+
+            MakeList GetListObject = new MakeList();
+            MachineClass GetmachineObject = new MachineClass();
+            var MakananList = GetListObject.MakananList();
+            var PecahanList = GetListObject.PecahanUangList();
+
             do
             {
-                Console.WriteLine("Hallo farhan");
-                int val = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Hallo farhan" + (cek - val));
-                Console.Write("diulang lagi y or n ");
-                key = Console.ReadLine();
-                
-            } while (key.Equals("y") ? true : false);
-            Console.WriteLine("Bye Bye");
+                Console.WriteLine("Menu Makanam");
+                foreach (var item in MakananList)
+                {
+                    status = item.stok > 0 ? "":"Stok Habis"; 
+                    Console.WriteLine("|| "+item.id+" || "+item.makanan+" || "+item.harga+" || "+ status);
+                }
+
+                #region Pemesanan
+                Console.WriteLine();
+                Console.Write("Pilih Nomor Makanan : ");
+                NoMakanan = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Jumlah Pembelian : ");
+                JmlPembelian = Convert.ToInt32(Console.ReadLine());
+                status = GetmachineObject.PengecekanStok(MakananList, NoMakanan, JmlPembelian);
+                Console.WriteLine(status);
+                #endregion
+                if(!status.Equals("Stok Makanan Kurang"))
+                {
+                    #region Konfirmasi
+                    Console.WriteLine();
+                    Console.Write("Lanjutkan Pemesanan y/n : ");
+                    condition = Console.ReadLine().Equals("y") ? true : false;
+                    if (condition)
+                    {
+                        #region Pembelian
+                        Console.WriteLine();
+                        Console.WriteLine("Pembayaran harus dengan pecahan : ");
+                        foreach (var item in PecahanList)
+                        {
+                            Console.WriteLine("|| " + item.pecahan + " || ");
+                        }
+
+                        Console.WriteLine();
+
+                        do
+                        {
+                            Console.Write("Masukan uang anda : ");
+                            var getuang = Convert.ToInt32(Console.ReadLine());
+                            if (GetmachineObject.PengecekanUang(PecahanList, getuang))
+                            {
+                                Uang += getuang;
+                                TotalPemesanan = GetmachineObject.TotalPemesanan(MakananList, NoMakanan, JmlPembelian);
+                                condition = TotalPemesanan > Uang ? true : false;
+                                Console.WriteLine("Total Uang : " + Uang);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Pecahan Uang Anda Tidak Di Kenal");
+                            }
+
+                        } while (condition);
+
+                        Console.WriteLine();
+                        GetmachineObject.Report(MakananList, NoMakanan, JmlPembelian, Uang);
+                        MakananList = GetmachineObject.UpdateStok(MakananList, NoMakanan, JmlPembelian);
+                        #endregion
+
+                    }
+                    #endregion
+                }
+
+                Console.WriteLine();
+                Console.Write("Lakukan Pemesanan Lagi y/n : ");
+                condition = Console.ReadLine().Equals("y") ? true : false;
+                Console.WriteLine();
+
+            } while (condition);
+            Console.WriteLine("Terima Kasih");
             Environment.Exit(0);
         }
     }
